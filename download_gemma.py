2c33aee39c9122cbff2930d61944edbefcc8a8dc
Shail423 <shaildesai592@gmail.com>
2025-07-28 12:17:46 +0530

    Update download_gemma.py

--- download_gemma.py
+++ download_gemma.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # Model ID for Gemma
-model_id = "google/distil-gemma-2b" 
+model_id = "google/gemma-3n-e2b-it"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id)
