114e2be7b629e4a2aee5fcabb72175f206d26244
Shail423 <shaildesai592@gmail.com>
2025-07-29 11:41:23 +0530

    Update and fix code functionality

--- download_distilgpt2.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="distilgpt2",
-    local_dir="./distilgpt2",
-    local_dir_use_symlinks=False  # optional, will ignore in recent versions
-)
-print("Download complete.")
